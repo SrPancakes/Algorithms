08b8753da31f19c58eca0e5fcdf85e75421172e9
SrPancakes <adormilucasnunez@gmail.com>
2023-09-25 16:27:56 +0200

    Corrección test tiempos

--- labAS1/testTiempos.c
+++ labAS1/testTiempos.c
@@ -7,9 +7,31 @@
 void testTiempos() {
     int *rv;
     int n;
-    double tiempo_inicio, tiempo_fin, tiempo_total;
+    double tiempo_inicio, tiempo_fin, tiempo_total, tiempo1, tiempo2;
 
-    printf("SumaSubMax 1\n"
+    printf("Test Tiempos SumaSubMax\n"
+           "%5s %15s %15s\n",
+           "n", "SumaSubMax 1", "SumaSubMax 2");
+    for (n = 500; n <= 32000; n *= 2) {
+        rv = malloc(sizeof (int) * n);
+        aleatorio(rv, n);
+
+        tiempo_inicio = microsegundos();
+        sumaSubMaxCuad(rv, n);
+        tiempo_fin = microsegundos();
+        tiempo1 = tiempo_fin - tiempo_inicio;
+
+        tiempo_inicio = microsegundos();
+        sumaSubMaxLineal(rv, n);
+        tiempo_fin = microsegundos();
+        tiempo2 = tiempo_fin - tiempo_inicio;
+
+        printf("%5d %15.3f %15.3f\n",
+               n, tiempo1, tiempo2);
+        free(rv);
+    }
+
+    printf("\nSumaSubMax 1\n"
            "%5s %15s %15s %15s %15s\n",
            "n", "t(n)", "t(n)/n^1.8", "t(n)/n^2.0", "t(n)/n^2.2");
     for (n = 500; n <= 32000; n *= 2) {
@@ -35,7 +57,8 @@ void testTiempos() {
         aleatorio(rv, n);
         sumaSubMaxLineal(rv, n);
         tiempo_fin = microsegundos();
-        printf("%5d %15.3f %15f %15f %15.7f\n",
+        tiempo_total = tiempo_fin - tiempo_inicio;
+        printf("%5d %15.3f %15.8f %15.9f %15.10f\n",
                n, tiempo_total, tiempo_total/pow(n, 1.8),
                tiempo_total/pow(n, 2.0), tiempo_total/pow(n, 2.2));
         free(rv);
